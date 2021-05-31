99c8b0d8837df306dd87f481333e543dafd88e4e
aaronBioBot <robotmessenger810@gmail.com>
2021-05-31 19:31:12 +0000

    test: firestore

--- root/tests.py
+++ root/tests.py
@@ -1,3 +1,4 @@
+import google.api_core.exceptions
 from django.test import TestCase, RequestFactory
 from root.utils import Firestore
 
@@ -19,6 +20,17 @@ class FirestoreTest(TestCase):
     def setUp(self):
         self.db = Firestore('file-sorting-root-tracking')
 
-    def test_query(self):
+    def test_get(self):
         doc = self.db.get('root', '2460')
         assert doc is not None
+    def test_invalid_get(self):
+        doc = self.db.get('root', 'invalid')
+        assert doc is None
+
+    def test_update(self):
+        self.assertRaises(google.api_core.exceptions.NotFound,
+                          self.db.update, 'robot-run-sample', 'invalid', {'example': 10})
+    def test_invalid_update(self):
+        self.db.update('robot-run-sample', 'box-example', {'example': 10})
+    def test_put(self):
+        self.db.put('robot-run-sample', 'box-example', {'example': 10}, True)
