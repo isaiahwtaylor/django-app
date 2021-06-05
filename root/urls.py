feba71a167e1e73a65429b838a895cb448c727c5
aaronBioBot <robotmessenger810@gmail.com>
2021-06-05 04:25:45 +0000

    remove unused

--- root/urls.py
+++ root/urls.py
@@ -4,8 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='name=root-home'),
     path('images/', views.images, name='name=root-images'),
-    # path('dashboard/', views.dashboard, name='name=root-dashboard'),
     path('internal/', views.internal, name='name=root-internal'),
     path('blog/', views.blog, name='name=root-blog')
-
 ]
