d14b5f571c45ad878541293d6213adae9a6a617d
aaronBioBot <robotmessenger810@gmail.com>
2021-07-06 04:02:07 +0000

    feat: signedurl + caching, on login

--- root/utils.py
+++ root/utils.py
@@ -1,6 +1,16 @@
+"""
+    Module consisting of helper classes to support django server logic
+"""
+
 import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
+from firebase_admin import credentials, firestore
+from google.cloud import storage
+
+import datetime
+import dateutil.parser
+from urllib import parse
+
+from django.utils import timezone
 
 
 class Firestore:
@@ -31,10 +41,105 @@ class Firestore:
         doc_ref = self.db.collection(collection).document(doc)
         doc_ref.update(data)
 
-    def query(self, collection, key, value):
-        return self.db.collection(collection).where(key, "==", value).stream()
+    def query(self, collection, key, value, comparator):
+        return self.db.collection(collection).where(key, comparator, value).stream()
 
-# class Bucket:
-#
-#     def __init__(self):
-#         pass
+class Bucket:
+
+    def __init__(self, bucket_name):
+        self.storage_client = storage.Client()
+        self.bucket = self.storage_client.bucket(bucket_name)
+
+    def generate_download_signed_url_v4(self, blob_name, time_valid_min):
+        """Generates a v4 signed URL for downloading a blob.
+
+        Note that this method requires a service account key file. You can not use
+        this if you are using Application Default Credentials from Google Compute
+        Engine or from the Google Cloud SDK.
+        """
+        # bucket_name = 'your-bucket-name'
+        # blob_name = 'your-object-name'
+
+        blob = self.bucket.blob(blob_name)
+
+        url = blob.generate_signed_url(
+            version="v4",
+            # This URL is valid for 15 minutes
+            expiration=datetime.timedelta(minutes=time_valid_min),
+            # Allow GET requests using this URL.
+            method="GET",
+        )
+        return url
+
+
+
+class ImageContext:
+    """
+        Data model to describe an image on the /gallery page
+    """
+
+    def __init__(self, url, box_num, seed_num):
+        # instance attributes
+        self.url = url
+        self.box_num = box_num
+        self._seed_num = seed_num
+
+    def is_url_expired(self) -> bool:
+        expires_seconds = parse.parse_qs(parse.urlparse(self.url).query)['X-Goog-Expires'][0]
+        start_date_iso = parse.parse_qs(parse.urlparse(self.url).query)['X-Goog-Date'][0]
+        expire_date_iso = dateutil.parser.parse(start_date_iso) + datetime.timedelta(
+            seconds=float(expires_seconds))
+        return timezone.now() > expire_date_iso
+
+    def get_exp_id(self) -> str:
+        return f'{self.box_num}.{self.seed_num}'
+
+    @staticmethod
+    def as_exp_id(box_num, seed_num) -> str:
+        return f'{box_num}.{seed_num}'
+
+    @classmethod
+    def from_exp_id(cls, url, exp_id):
+        return cls(url, exp_id.split['.'][0], exp_id.split['.'][1])
+
+    @property
+    def seed_num(self):
+        return self._seed_num
+
+    @seed_num.setter
+    def seed_num(self, value):
+        if int(value) > 0 and isinstance(value, str):
+            self._seed_num = value
+        else:
+            raise ValueError('seed_num value must be > 0')
+
+    def to_dict(self) -> dict:
+        return {'url': self.url, 'box_num': self.box_num, 'seed_num': self.seed_num}
+
+class Cache:
+    """
+        A dictionary implementation to cache signed_urls of ImageContext data
+    """
+
+    # class attributes
+    bucket = Bucket('file-sorting-data')
+    bucket_path = 'Anupam/showcase/showcase/{}/hist.png'
+
+    def __init__(self):
+        # instance attributes
+        self.signed_urls = {}
+
+    def get(self, box_num, seed_num) -> ImageContext:
+        return self.signed_urls.get(ImageContext.as_exp_id(box_num, seed_num))
+
+    def contains(self, box_num, seed_num) -> bool:
+        return ImageContext.as_exp_id(box_num, seed_num) in self.signed_urls
+
+    def is_expired(self, box_num, seed_num) -> bool:
+        return self.get(box_num, seed_num).is_url_expired()
+
+    def update(self, box_num, seed_num) -> ImageContext:
+        url = self.bucket.generate_download_signed_url_v4(self.bucket_path.format(box_num), 10)
+        context = ImageContext(url, box_num, seed_num)
+        self.signed_urls[context.get_exp_id()] = context
+        return context
--- root/views.py
+++ root/views.py
@@ -1,14 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from root.utils import Firestore
-from google.cloud import storage
+from root.utils import Firestore, Bucket, ImageContext, Cache
 import os
 
+from urllib import parse
+import dateutil.parser
+import datetime
+from django.utils import timezone
+
 db = Firestore("file-sorting-root-tracking")
+b = Bucket('file-sorting-data')
+
 
 prefixes = ["2460", "2461", "2462", "2463", "2464", "2465"]
 delimiter = '/'
-storage_client = storage.Client()
+
+# find element in unordered list O(n), find element in set is O(1)
+# set does not have key:value pairs
+# no literals for set, {} reserved for dict
+# signed_urls = {}
+cache = Cache()
+
 
 
 def home(request):
@@ -33,6 +45,33 @@ def images(request):
         context = {'exp': doc, 'seed': seed_context}
 
         return render(request, 'root/image-details.html', context)
+    elif request.GET.get('date') is not None and request.GET.get('date') is not "":
+        docs = db.query('root', 'date_planted', request.GET.get('date').replace('-', '/'), '>=')
+        image_urls = []
+        for doc in docs:
+            seed_count = len(doc.to_dict().get('seeds'))
+            for x in range(seed_count):
+                image_urls.append({"url":
+                                       f"https://storage.googleapis.com/root-tracking-public/showcase/{doc.id}/"
+                                       f"germination_seed{str(x + 1)}.png",
+                                   "box_num": doc.id, "seed_num": str(x + 1)})
+
+        if request.user.is_superuser:
+            # TODO loop through experiments in private bucket
+            boxes = [{'box_num': '2460', 'seed_num': '1'}, {'box_num': '2461', 'seed_num': '1'}]
+            for box in boxes:
+                box_num = box.get('box_num')
+                seed_num = box.get('seed_num')
+
+                if cache.contains(box_num, seed_num) and not cache.is_expired(box_num, seed_num):
+                    image_context = cache.get(box_num, seed_num)
+                else:
+                    image_context = cache.update(box_num, seed_num)
+                image_urls.append(image_context.to_dict())
+                print(cache.get(box_num, seed_num).to_dict())
+
+        context = {'image_urls': image_urls}
+        return render(request, 'root/images.html', context)
 
     context = {'image_urls': get_image_urls()}
 
@@ -40,7 +79,10 @@ def images(request):
 
 
 def internal(request):
-    return render(request, 'root/internal.html')
+    if request.user.is_superuser:
+        return render(request, 'root/internal.html')
+    else:
+        return redirect('/login')
 
 
 def blog(request):
@@ -51,7 +93,7 @@ def get_image_urls():
     image_urls = []
     for prefix in prefixes:
 
-        blobs = storage_client.list_blobs('root-tracking-public', prefix=f'showcase/{prefix}/', delimiter=delimiter)
+        blobs = b.storage_client.list_blobs('root-tracking-public', prefix=f'showcase/{prefix}/', delimiter=delimiter)
 
         for blob in blobs:
             # is_public = "READER" in blob.acl.all().get_roles()
