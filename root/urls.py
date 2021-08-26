fa82a71893ba85bcf870a1ac13830a26ce7ac530
aaronBioBot <robotmessenger810@gmail.com>
2021-08-26 02:52:13 +0000

    refactor: images/ to gallery/

--- root/urls.py
+++ root/urls.py
@@ -4,9 +4,10 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='name=root-home'),
-    path('images/', views.images, name='name=root-images'),
+    path('gallery/', views.gallery, name='name=root-gallery'),
     path('internal/', views.internal, name='name=root-internal'),
     path('blog/', views.blog, name='name=root-blog'),
     path('login/', auth_views.LoginView.as_view(template_name='root/login.html'), name='name=root-login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='root/logout.html'), name='name=root-logout')
+    path('logout/', auth_views.LogoutView.as_view(template_name='root/logout.html'), name='name=root-logout'),
+    path('about/', views.about, name='name=root-about')
 ]
